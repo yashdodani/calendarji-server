6b2a248b0f2c92830a6eede79a754aaf54f1600d
Yash Dodani <yashdodani8@gmail.com>
2024-12-11 16:16:32 +0530

    redirect issue 5

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-import 'dotenv/config'
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-import calendarRoutes from './routes/calendarRoutes.js'
-import authRoutes from './routes/authRoutes.js'
-import oauth2Client from './utils/oauth2Client.js'
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import calendarRoutes from "./routes/calendarRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import oauth2Client from "./utils/oauth2Client.js";
 
 const app = express();
 
-app.use(cors({}));
+app.use(
+    cors({
+        origin: "https://calendarji.vercel.app",
+        credentials: true,
+    })
+);
 
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/calendar', calendarRoutes);
-app.use('/auth', authRoutes);
+app.use("/calendar", calendarRoutes);
+app.use("/auth", authRoutes);
 
-app.get('/events', (req, res) => {
-})
+app.get("/events", (req, res) => {});
 
 export default app;
