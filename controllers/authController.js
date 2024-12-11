bc04703cd7c6c7a0d03f39ba0de71f4387844dab
Yash Dodani <yashdodani8@gmail.com>
2024-12-11 16:07:29 +0530

    redirect issue 3

--- controllers/authController.js
+++ controllers/authController.js
@@ -152,7 +152,7 @@ export async function eventsRedirect(req, res) {
 
         res.cookie("jwt", jwt_token, {
             maxAge: 24 * 60 * 60 * 1000,
-            httpOnly: true,
+            httpOnly: false,
         });
         console.log(`Redirecting to ${redirectUrl}`);
         res.redirect(redirectUrl);
