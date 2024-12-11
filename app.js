bfdd295cbe6015ec061c04b80fa4de88e9a6bb03
Yash Dodani <yashdodani8@gmail.com>
2024-12-11 20:23:30 +0530

    corrected api 3

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: "https://calendarjiapi.vercel.app",
+        origin: "https://calendarji.vercel.app",
         // origin: "http://localhost:5173",
         credentials: true,
     })
